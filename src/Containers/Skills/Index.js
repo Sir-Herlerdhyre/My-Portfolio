577242fc62a3dffd7661342544c12df3888490f7
Aladire Soliu <soliualadire@gmail.com>
2025-07-02 00:19:15 +0100

    skill update and responsiveness

--- src/Containers/Skills/Index.js
+++ src/Containers/Skills/Index.js
@@ -10,6 +10,7 @@ import './Styles.css';
 // Skill data
 const skillsData = [
   {
+    Label : "FRONTEND SKILLS",
     data: [
       { skillName: "HTML", percentage: "90" },
       { skillName: "CSS", percentage: "80" },
@@ -18,6 +19,14 @@ const skillsData = [
       { skillName: "REACT", percentage: "75" },
     ],
   },
+
+  {
+    Label : "BACKEND SKILLS",
+    data: [
+      {skillName : "NODE.JS", percentage : "50"}
+    ]
+
+  }
 ];
 
 const Skills = () => {
@@ -31,29 +40,30 @@ const Skills = () => {
 
       {/* Skills container */}
       <div className="skills-section animated-fadeInUp">
-        <h3 className="title">FRONTEND SKILLS</h3>
-
-        {/* Loop through and display progress bars */}
+        
         <div className="progress-bar-container">
-          {skillsData.map((item, index) =>
-            item.data.map((skillItem, i) => (
-              <div className="progress-bar" key={`${index}-${i}`}>
-                <p>{skillItem.skillName}</p>
-                <div className="progress-wrapper">
-                  <Line
-                    percent={skillItem.percentage}
-                    strokeWidth="2"
-                    strokeColor="black"
-                    trailWidth="2"
-                    strokeLinecap="square"
-                  />
-                  <span className="progress-label">
-                    {skillItem.percentage}%
-                  </span>
-                </div>
-              </div>
-            ))
-          )}
+          {skillsData.map((item, index) => ( <div key={index} className="skill-group">
+            
+    <h3 className="skill-category">{item.Label}</h3>
+    {item.data.map((skillItem, i) => (
+      <div className="progress-bar" key={skillItem.skillName}>
+        <div className="progress-info">
+          <p>{skillItem.skillName}</p>
+          <span className="progress-label">{skillItem.percentage}%</span>
+        </div>
+        <Line
+          percent={skillItem.percentage}
+          strokeWidth="2"
+          strokeColor="black"
+          trailWidth="2"
+          strokeLinecap="square"
+        />
+      </div>
+    ))}
+  </div>
+))}
+
+
         </div>
       </div>
     </section>
